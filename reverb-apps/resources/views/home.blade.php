9c9fccfe329bfa2b470fc33839c4c1cde1865ab2
Luiz Fernando de Santana <luizfernando.itpro@gmail.com>
2021-01-05 09:49:38 -0300

    criada a homepage

--- reverb-apps/resources/views/home.blade.php
+++ reverb-apps/resources/views/home.blade.php
@@ -1,5 +1,13 @@
 @include("shared/header")
             
-    
+   <h2>Bem-vindo as Ferramentas Online de Desenvolvimento da Reverb</h2> 
+   <p>
+    As ferramentas Online de Desenvolvimento da 
+    <a href="http://www.reverb.tec.br/">Reverb</a> 
+    existem para ajudar a todos com ferramentas online e gratuitas. 
+    Fácil, simples, sem complicações. 
+    Estamos implementando sempre que possível as ferramentas.
+    Se sentir falta de alguma ferramenta, por favor <a href="#">fale conosco</a>.
+   </p>
 
 @include("shared/footer")
