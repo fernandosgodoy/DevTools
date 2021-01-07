ca3343fea8b12a806570f9ac78bf2072e7505aa4
Luiz Fernando de Santana <luizfernando.itpro@gmail.com>
2021-01-07 12:09:34 -0300

    função de Uppercase concluida

--- reverb-apps/resources/views/string-functions/transformacaoString.blade.php
+++ reverb-apps/resources/views/string-functions/transformacaoString.blade.php
@@ -5,16 +5,17 @@
     </div>
 
     <div id="error_toUpperCaseString" class="result-error hide">
-        <span>Preencha corretamente o campo acima.</span>
+        <span>Preencha corretamente o campo abaixo.</span>
     </div>
     <br class="clear" />
 
-    <textarea id="result_toUpperCaseString_txt" class="form-control textarea-input" cols="50" rows="20"
+    <textarea id="result_toUpperCaseString_txt" 
+        class="form-control textarea-input" rows="10"
         placeholder="cole seu texto aqui e clique no botão para exibir o resultado"></textarea>
   	<br class="clear" />
       <button id="btnUpperCaseString" type="button"
-        class="btn btn-dark">Transformar</button>
+        class="btn btn-dark">UPPERCASE</button>
         <button id="btnLimparUpperCaseString" type="button"
-        class="btn">Limpar e fazer nova consulta</button>
+        class="btn">Limpar Campo</button>
 
 @include("shared/footer")
