8060985d81dd0d3a85050e5d12cf9fa17841802e
Luiz Fernando de Santana <luizfernando.itpro@gmail.com>
2021-01-18 18:58:42 -0300

    Incluido mais textos de referência para o site FS Developer

--- reverb-apps/resources/views/home.blade.php
+++ reverb-apps/resources/views/home.blade.php
@@ -2,12 +2,24 @@
             
    <h2>Bem-vindo as Ferramentas Online de Desenvolvimento da Reverb</h2> 
    <p>
-    As ferramentas Online de Desenvolvimento da 
-    <a href="http://www.reverb.tec.br/">Reverb</a> 
-    existem para ajudar a todos com ferramentas online e gratuitas. 
-    Fácil, simples, sem complicações. 
-    Estamos implementando sempre que possível as ferramentas.
-    Se sentir falta de alguma ferramenta, por favor <a href="http://www.reverb.tec.br/contato">entre em contato</a>.
+      As ferramentas Online de Desenvolvimento da 
+      <a href="http://www.reverb.tec.br/">Reverb</a> 
+      existem para ajudar a todos com ferramentas online e gratuitas. 
+   </p>
+   <p>
+      Fácil, simples, sem complicações. 
+      Estamos implementando sempre que possível as ferramentas.
+      Se sentir falta de alguma ferramenta, por favor <a href="http://www.reverb.tec.br/contato">entre em contato</a>.
+   </p>
+   <p>
+      Se você tem interesse em desenvolver seu projeto conosco e quer conhecer mais 
+      sobre os meus conhecimentos da área, recomendo a leitura em meu site 
+      <a href="https://www.fsdeveloper.com.br/" target="_blank">FS Developer</a>, onde 
+      posto semanalmente artigos técnicos de tecnologias que estou aprendendo e que já
+      domino.
+   </p>
+   <p>
+      
    </p>
 
 @include("shared/footer")
